9a89a5e59e03ad5dabd94b50f4c52035cc4b65bb
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-12-30 12:18:34 +0600

    fixed schema typo

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import prisma from "@/prisma/prisma";
-const createIssueScema = z.object({
+const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
 });
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueScema.safeParse(body);
+  const validation = createIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 500 });
   }

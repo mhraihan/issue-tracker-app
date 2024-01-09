d8c06a57a89826dfc18942b9c7bb3cabf3f2b99a
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-09 22:06:08 +0600

    change to return type

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,19 +1,16 @@
 import { issueSchema } from "@/app/validationSchemas";
-import { auth } from "@/auth";
 import prisma from "@/prisma/prisma";
-import { NextResponse } from "next/server";
+import { checkAuthorization, handleUnauthorized } from "@/utils/authUtils";
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const isLoggedIn = await checkAuthorization();
 
-export const POST = auth(async (request) => {
-  if (!request?.auth) {
-    return NextResponse.json(
-      { message: "Unauthorize Access" },
-      { status: 401 }
-    );
+  if (!isLoggedIn) {
+    return handleUnauthorized();
   }
 
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
-
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 500 });
   }
@@ -25,5 +22,29 @@ export const POST = auth(async (request) => {
     },
   });
 
-  return NextResponse.json(newIssue, { status: 201 }) as Response;
-});
+  return NextResponse.json(newIssue, { status: 201 });
+}
+// export const POST = auth(async (request) => {
+//   if (!request?.auth) {
+//     return NextResponse.json(
+//       { message: "Unauthorize Access" },
+//       { status: 401 }
+//     );
+//   }
+
+//   const body = await request.json();
+//   const validation = issueSchema.safeParse(body);
+
+//   if (!validation.success) {
+//     return NextResponse.json(validation.error.format(), { status: 500 });
+//   }
+
+//   const newIssue = await prisma.issue.create({
+//     data: {
+//       title: body.title,
+//       description: body.description,
+//     },
+//   });
+
+//   return NextResponse.json(newIssue, { status: 201 }) as Response;
+// });

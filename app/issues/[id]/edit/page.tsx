e946cbbd0dc3e977e5c9a10123a8fad3d561daf0
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-09 12:49:41 +0600

    Optimize performance using react cache

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -2,17 +2,19 @@ import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
 import IssueFormSkeleton from "./../../_components/IssueFormSkeleton";
 import { findIssueById } from "@/utils/prismaUtils";
+import { cache } from "react";
 interface Props {
   params: {
     id: string;
   };
 }
+const fetchIssue = cache((id: string) => findIssueById(id))
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
   loading: () => <IssueFormSkeleton />,
 });
 const EditIssuePage = async ({ params: { id } }: Props) => {
-  const issue = await findIssueById(id);
+  const issue = await fetchIssue(id);
 
   if (!issue) {
     notFound();
@@ -20,7 +22,7 @@ const EditIssuePage = async ({ params: { id } }: Props) => {
   return <IssueForm issue={issue} />;
 };
 export async function generateMetadata({params: {id}}: Props){
-  const issue = await findIssueById(id);
+  const issue = await fetchIssue(id);
   return {
     title: `Edit: ${issue?.title} - Issue Tracker`,
     description: `Edit Issue ${issue?.id}`
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,22 +2,23 @@ import { checkAuthorization } from "@/utils/authUtils";
 import { findIssueById } from "@/utils/prismaUtils";
 import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import { cache } from "react";
+import AssigneeSelect from "./AssigneeSelect ";
 import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import AssigneeSelect from "./AssigneeSelect ";
 interface Props {
   params: {
     id: string;
   };
 }
+const fetchIssue = cache((id: string) => findIssueById(id));
 const IssueDetailsPage = async ({ params: { id } }: Props) => {
   try {
     const [session, issue] = await Promise.all([
       checkAuthorization(),
-      findIssueById(id),
+      fetchIssue(id),
     ]);
-
 
     if (!issue) {
       notFound();
@@ -43,11 +44,11 @@ const IssueDetailsPage = async ({ params: { id } }: Props) => {
   }
 };
 
-export async function generateMetadata({params:{id}}: Props){
-  const issue =   await findIssueById(id)
+export async function generateMetadata({ params: { id } }: Props) {
+  const issue = await fetchIssue(id);
   return {
     title: issue?.title,
-    description: `Details of issue ${issue?.id}`
-  }
+    description: `Details of issue ${issue?.id}`,
+  };
 }
 export default IssueDetailsPage;

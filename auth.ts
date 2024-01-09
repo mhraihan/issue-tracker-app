2283e5e0e82fd7fc46f67e9cdf65e8cdea9fd2ee
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2024-01-09 19:06:04 +0600

    Github Provider added

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 export const authConfig = {
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [Github, Google],
   session: {
     strategy: "jwt",
   },
